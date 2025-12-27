ae7dc26c6824d394d017172abbdb0f7ec5080f40
Faris Ansari <faristheknight77@gmail.com>
2025-12-27 18:45:32 +0400

    Added logic for the pacet dropping and the error handling

--- main.cpp
+++ main.cpp
@@ -4,13 +4,38 @@
 #include "ftxui/component/screen_interactive.hpp"
 #include "ftxui/dom/elements.hpp"
 
-#include "windivert.h"
-
+#include <winsock2.h>
+#include <stdio.h>
 #include <memory>
 #include <string>
+#include <thread>
+#include <atomic>
+
+#include "windivert.h"
+#include <windows.h>
+
 
 using namespace ftxui;
 
+HANDLE g_hFilter = INVALID_HANDLE_VALUE;
+
+void startDroppingPackets() {
+  g_hFilter = WinDivertOpen("inbound", WINDIVERT_LAYER_NETWORK, 0, WINDIVERT_FLAG_DROP);
+
+  if (g_hFilter == INVALID_HANDLE_VALUE) {
+    MessageBox(
+      NULL, 
+      std::to_string(GetLastError()).c_str(),
+      "SWITCHEROO - Error",
+      MB_OK | MB_ICONINFORMATION
+    );
+  }
+}
+
+void stopDroppingPackets() {
+  WinDivertClose(g_hFilter);
+  g_hFilter = INVALID_HANDLE_VALUE;
+}
 
 ButtonOption Style() {
   auto option = ButtonOption::Animated();
@@ -26,16 +51,17 @@ ButtonOption Style() {
   return option;
 }
 
-
 void toggleSwitch(bool& state, Color& color, std::string& switchText) {
   state = !state;
 
   if (state == true) {
     color = Color::Green;
     switchText = "On";
+    startDroppingPackets();
   } else {
     color = Color::Red;
     switchText = "Off";
+    stopDroppingPackets();
   }
 }
 
@@ -49,7 +75,7 @@ int main() {
 
   auto component = Renderer(lag_button, [&] {
     return vbox({
-      text("SWITCHEROO!") | bold | center,
+      text("SWITCHEROO! v1.0") | bold | center,
       separator(),
       text("Switch: " + switchText) | center | color(switchColor),
       separator(),
@@ -59,6 +85,7 @@ int main() {
 
   auto screen = ScreenInteractive::FitComponent();
   screen.Loop(component);
-
+  
+  stopDroppingPackets();
   return 0;
 }
